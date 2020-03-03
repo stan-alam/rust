32998aa9412ba41415e1f5e84fa748785b67bc8a
root <root@LAPTOP-STANPC.localdomain>
2020-03-02 20:33:10 -0500

    added screencap for cargo install proj, and cargo run, updated .gitignore for Rust

---
+++ CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/HelloMultiverse.d
@@ -0,0 +1,1 @@
+/mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/HelloMultiverse: /mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/src/main.rs
---
+++ CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/deps/HelloMultiverse-c3f9e0eda48df81a.d
@@ -0,0 +1,5 @@
+/mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/deps/HelloMultiverse-c3f9e0eda48df81a: src/main.rs
+
+/mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/deps/HelloMultiverse-c3f9e0eda48df81a.d: src/main.rs
+
+src/main.rs:
