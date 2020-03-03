b67925f5d8fb5ef561a9c1bae0352169fe7035d2
root <root@LAPTOP-STANPC.localdomain>
2020-03-02 20:38:15 -0500

    ran cargo clean to remove binaries

--- CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/HelloMultiverse.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/HelloMultiverse: /mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/src/main.rs
--- CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/deps/HelloMultiverse-c3f9e0eda48df81a.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/deps/HelloMultiverse-c3f9e0eda48df81a: src/main.rs
-
-/mnt/c/git/rust/CoreRust/ProgRust/images/01/src/HelloMultiverse/target/debug/deps/HelloMultiverse-c3f9e0eda48df81a.d: src/main.rs
-
-src/main.rs:
